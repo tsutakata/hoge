f1706cfe7917e4e2a00e10acfc7e7d71c8cee0a9
tsutakata <akt216@gmail.com>
2018-11-15 21:42:08 +0900

    initial commit.

---
+++ src/main/java/com/example/demo/HogehogeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogehogeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogehogeApplication.class, args);
+	}
+}
